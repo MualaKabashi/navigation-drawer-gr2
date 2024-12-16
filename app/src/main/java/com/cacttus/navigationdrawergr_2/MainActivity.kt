eeb8a806220cc77b109d4a0c6a0656b08c318b0e
Muala Kabashi <keds30955@KEDS.kv>
2024-12-16 12:43:37 +0100

    first commit

---
+++ app/src/main/java/com/cacttus/navigationdrawergr_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cacttus.navigationdrawergr_2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
+
